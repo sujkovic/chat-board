ee9a1d9454d68f4963e755c9bf94b59b4f26b031
sujkovic <adrian.sujkovic@gmail.com>
2023-01-14 15:24:28 -0500

    functionality done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Heyyyy",
+    user: "Donald",
+    added: new Date(),
+  },
+  {
+    text: "How ya doinnn",
+    user: "Joe",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
